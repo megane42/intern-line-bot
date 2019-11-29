3fa9a05701965a451839a295e61abf277265690d
megane42 <h.kzm.gm@gmail.com>
2019-11-29 15:26:43 +0900

    user モデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2014_07_07_111715) do
+ActiveRecord::Schema.define(version: 2019_11_29_061312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_users_on_uid", unique: true
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string "name"
